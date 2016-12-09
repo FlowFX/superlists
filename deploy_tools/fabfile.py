dc3b184046c213cca3abc7332bcca655ed8d134c
Florian Posdziech <hallo@flowfx.de>
2016-12-09 10:23:55 -0600

    bugfix

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,7 +2,7 @@ from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 import random
 
-REPO_URL = 'https://github.com/flowfx/book-example.git'
+REPO_URL = 'https://github.com/flowfx/superlists.git'
 
 def deploy():
     site_folder = '/home/%s/sites/%s' % (env.user, env.host)   
